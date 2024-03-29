f383803dd33423833e7587002dc19fd7a9915f88
dovanphuong2704 <dophuong27423@gmail.com>
2024-03-29 11:36:13 +0700

    add update button and update quantity list shop product

--- src/components/items/CartItem.tsx
+++ src/components/items/CartItem.tsx
@@ -10,6 +10,9 @@ interface ShopItemProps {
 const CartItem = ({ dataCart }: ShopItemProps) => {
     const [cartProducts, setCartProducts] = useLocalStorage(LocalStorageKeys.CART_PRODUCTS)
     const [shopProducts, setShopProducts] = useLocalStorage(LocalStorageKeys.SHOP_PRODUCTS)
+    const [newQuantity, setNewQuantity] = useState<number>(dataCart.quantity)
+    const [remainingQuantity, setRemainingQuantity] = useState<number>(0)
+
 
     const shopItem: IProduct = useMemo(() => {
         const DUMMY_DATA = {
@@ -27,24 +30,45 @@ const CartItem = ({ dataCart }: ShopItemProps) => {
         }
         return DUMMY_DATA
     }, [dataCart, shopProducts])
-
-    const updateQuantity = (newQuantity: number) => {
-        const idx = cartProducts?.findIndex(item => item.id == dataCart.id)
-        if (idx == undefined) return;
-
-        const newCart = JSON.parse(JSON.stringify([...cartProducts || []]));
-        newCart[idx].quantity = newQuantity;
-        setCartProducts(newCart);
-    };
-
-
     const handleDelete = () => {
         if (cartProducts !== null) {
             const updatedCartProducts = cartProducts.filter(product => product.id !== dataCart.id);
             setCartProducts(updatedCartProducts);
         }
     }
+    const handleUpdate = () => {
+        if (newQuantity <= shopItem.quantity) {
 
+            const updatedShopProducts = [...shopProducts || []]
+            const idx = updatedShopProducts.findIndex(product => product.id === dataCart.id);
+            const remainingQuantity = idx !== -1 ? updatedShopProducts[idx].quantity - newQuantity : 0;
+            if (idx !== -1) {
+                const updatedProductCopy = { ...updatedShopProducts[idx] };
+                updatedProductCopy.quantity = remainingQuantity;
+                updatedShopProducts[idx] = updatedProductCopy;
+                setShopProducts(updatedShopProducts);
+                setRemainingQuantity(remainingQuantity);
+                if (cartProducts !== null) {
+                    const updatedProductIndex = cartProducts.findIndex(product => product.id === dataCart.id);
+                    const updatedProduct = { ...cartProducts[updatedProductIndex] };
+                    updatedProduct.quantity = newQuantity;
+                    const updatedCartProducts = [...cartProducts];
+                    updatedCartProducts[updatedProductIndex] = updatedProduct;
+                    setCartProducts(updatedCartProducts);
+
+                }
+            }
+
+            alert("Đã cập nhật thành công")
+        } else {
+            alert("Không đủ số lượng hàng");
+        }
+    }
+
+
+    console.log("cart", dataCart.quantity);
+
+    console.log(newQuantity);
 
 
     return (
@@ -64,21 +88,26 @@ const CartItem = ({ dataCart }: ShopItemProps) => {
                     <td className={cx("cart-product-image-cell")}>
                         <img className={cx("cart-product-image")} src={dataCart.image} alt={dataCart.name} />
                     </td>
-                    <td className={cx("cart-product-name")}>{dataCart.name} <br /> Còn: {shopItem && shopItem?.quantity - dataCart.quantity} </td>
+                    <td className={cx("cart-product-name")}>
+                        {dataCart.name} <br />
+                        Còn: {shopItem.quantity}
+                    </td>
                     <td className={cx("cart-product-price")}>${dataCart.price}</td>
                     <td className={cx("cart-product-quantity")}>
-                    <div>{dataCart.quantity}</div>
-                                        <button className='buton-cart' onClick={() => updateQuantity(Math.max(dataCart.quantity - 1, 1))}>-</button>
-                                        <button className='buton-cart' onClick={() => {
-                                            if (dataCart.quantity >= shopItem.quantity) {
-                                                alert('Hết hàng!');
-                                            } else {
-                                                updateQuantity(dataCart.quantity + 1);
-                                            }
-                                        }}>+</button>
+                        <input
+                            type="number"
+                            id="quantity"
+                            name="quantity"
+                            value={newQuantity.toString()}
+                            onChange={(e) => setNewQuantity(parseInt(e.target.value))}
+                            min="1"
+
+                        />
                     </td>
                     <td className={cx("cart-product-total")}>${dataCart.price * dataCart.quantity}</td>
                     <td className={cx("cart-product-action")}>
+                        <button className={cx("button-cart-update")} onClick={handleUpdate}>update</button>
+
                         <button className={cx("button-cart-delete")} onClick={handleDelete}>Xóa</button>
                     </td>
                 </tr>
